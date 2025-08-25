bdb4fedb80c284e8d5e12e7ac5256e0412f5f8c8
Jangminez <111687963+Jangminez@users.noreply.github.com>
2025-08-25 21:15:46 +0900

    feat: 코드 추가
    적 상태, UI, Util, Vehicle, Weapon

--- Building/Dialogue/DialogueSequence.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewDialogueSequence", menuName = "Dialogue/Dialogue Sequence")]
-public class DialogueSequence : ScriptableObject
-{
-    [Header("대사 목록")]
-    public DialogueLine[] dialogueLines;
-}
